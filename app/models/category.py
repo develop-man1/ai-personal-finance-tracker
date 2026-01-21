40dc794e1e0c7279f02d556404dd2c14af0e2883
develop-man1 <smailhit2003@gmail.com>
2026-01-22 02:17:49 +0500

    finish model + alembic

--- app/models/category.py
+++ app/models/category.py
@@ -1,10 +1,18 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
 from ..database import Base
 
 
 class Category(Base):
-    __tablename__ = "category"
-    
+    __tablename__ = "categories"
+
     id = Column(Integer, primary_key=True, index=True)
-    
+    name = Column(String, nullable=False)
+    type = Column(String, nullable=False) # доход или расходы
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    user = relationship("User", back_populates="categories")
+    transactions = relationship("Transaction", back_populates="category", cascade="all, delete")
