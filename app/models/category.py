218fa6694e103dbcd72a1dd382d5ffe469568885
develop-man1 <smailhit2003@gmail.com>
2026-01-21 22:30:11 +0500

    add models/user

---
+++ app/models/category.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.orm import relationship
+from ..database import Base
+
+
+class Category(Base):
+    __tablename__ = "category"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    
