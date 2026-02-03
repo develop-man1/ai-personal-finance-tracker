ea306e1b53f1ea52ae2b801a58abe2bcf5dcbf36
develop-man1 <smailhit2003@gmail.com>
2026-02-04 02:08:20 +0500

    add services

---
+++ app/services/auth_service.py
@@ -0,0 +1,50 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status
+
+from app.models.user import User
+from app.repositories.user_repository import UserRepository
+from app.utils.security import get_password_hash, verify_password
+
+
+class AuthService:
+    
+    def __init__(self, db: Session):
+        self.repository = UserRepository(db)
+    
+    
+    def register_user(self, username: str, password: str) -> User:
+        
+        existing_user = self.repository.get_by_username(username)
+        
+        if existing_user:
+            raise HTTPException(status_code=400, detail="Username already exists")
+        
+        
+        hashed_password = get_password_hash(password)
+        
+        new_user = self.repository.create(username=username, hashed_password=hashed_password)
+        
+        return new_user
+    
+    
+    def authenticate_user(self, username: str, password: str) -> User:
+        
+        user = self.repository.get_by_username(username)
+        
+        if not user:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+            
+        password_is_correct = verify_password(password, user.hashed_password) # type: ignore
+        
+        if not password_is_correct:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid username or password",
+                headers={"WWW-Authenticate": "Bearer"},
+            )
+            
+        return user
