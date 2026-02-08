916e675caa034ae021351598285b2dae243c45e4
develop-man1 <smailhit2003@gmail.com>
2026-02-09 04:17:34 +0500

    add async alembic

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from app.config import settings
 from app.routes import auth_router, categories_router, transactions_router, users_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Старт
+    print("Программа запустилась")
+    
+    yield
+    # Отключение
+    print("Программа отключилась")
+
 
 app = FastAPI(
     title=settings.app_name,
     debug=settings.debug,
+    lifespan=lifespan,
 )
 
 app.include_router(auth_router)
@@ -12,6 +25,7 @@ app.include_router(categories_router)
 app.include_router(transactions_router)
 app.include_router(users_router)
 
+
 @app.get("/")
-def root():
+async def root():
     return {"status": "ok"}
