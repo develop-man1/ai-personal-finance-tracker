e54560520e2e9f8b8bfe85af1529b1556d9ff7d7
develop-man1 <smailhit2003@gmail.com>
2026-02-09 04:29:01 +0500

    add async run

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
+import asyncio
+import sys
 import uvicorn
 from app.config import settings
+
+if sys.platform == "win32":
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 if __name__ == '__main__':
     uvicorn.run(
