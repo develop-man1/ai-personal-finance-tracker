34821ea534bb8901d46f6f26c2d23b9d2372dbc0
develop-man1 <smailhit2003@gmail.com>
2026-02-09 03:53:36 +0500

    add async dependencies

--- app/utils/dependencies.py
+++ app/utils/dependencies.py
@@ -1,8 +1,7 @@
-from typing import Generator
-
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 
 from app.database import get_db
 from app.models.user import User
@@ -20,7 +19,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 # Current user dependency
 # =========================
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)) -> User:
     """
     Возвращает текущего авторизованного пользователя.
     
@@ -45,7 +44,10 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
         )
 
     # Ищем пользователя в БД
-    user = db.query(User).filter(User.username == username).first()
+    # user = db.query(User).filter(User.username == username).first()
+    stmt = select(User).where(User.username == username)
+    result = await db.execute(stmt)
+    user = result.scalar_one_or_none()
 
     if user is None:
         raise HTTPException(
