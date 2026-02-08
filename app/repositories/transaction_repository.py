84e1fa3ca3c50e5062809f34a9fa281b57a82068
develop-man1 <smailhit2003@gmail.com>
2026-02-09 03:21:04 +0500

    add async repositories and database

--- app/repositories/transaction_repository.py
+++ app/repositories/transaction_repository.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from typing import Optional, List
 from decimal import Decimal
 from datetime import datetime
@@ -7,24 +8,44 @@ from app.models.transaction import Transaction
 
 
 class TransactionRepository:
-    def __init__(self, db: Session):
+    def __init__(self, db: AsyncSession):
+        
         self.db = db
         
-    def get_by_id(self, transaction_id: int) -> Optional[Transaction]:
-        return self.db.query(Transaction).filter(Transaction.id == transaction_id).first()
+    async def get_by_id(self, transaction_id: int) -> Optional[Transaction]:
+        stmt = select(Transaction).where(Transaction.id == transaction_id)
+        result = await self.db.execute(stmt)
+        return result.scalar_one_or_none()
+        
     
-    def get_all_by_user(self, user_id: int) -> List[Transaction]:
-        return self.db.query(Transaction).filter(Transaction.user_id == user_id).all()
+    async def get_all_by_user(self, user_id: int) -> List[Transaction]:
+        # return self.db.query(Transaction).filter(Transaction.user_id == user_id).all()
+        stmt = select(Transaction).where(Transaction.user_id == user_id)
+        result = await self.db.execute(stmt)
+        return list(result.scalars().all())
     
-    def create(self, user_id: int, category_id: int, amount: Decimal, date: datetime, description=None,) -> Transaction:
+    async def create(self, user_id: int, category_id: int, amount: Decimal, date: datetime, description=None,) -> Transaction:
+        # new_transaction = Transaction(user_id=user_id, category_id=category_id, amount=amount, date=date, description=description)
+        # self.db.add(new_transaction)
+        # self.db.commit()
+        # self.db.refresh(new_transaction)
+        # return new_transaction
         new_transaction = Transaction(user_id=user_id, category_id=category_id, amount=amount, date=date, description=description)
         self.db.add(new_transaction)
-        self.db.commit()
-        self.db.refresh(new_transaction)
+        await self.db.commit()
+        await self.db.refresh(new_transaction)
         return new_transaction
 
-    def delete(self, transaction_id: int) -> None:
-        transaction = self.get_by_id(transaction_id)
+    async def delete(self, transaction_id: int) -> None:
+        # transaction = self.get_by_id(transaction_id)
+        # if transaction:
+        #     self.db.delete(transaction)
+        #     self.db.commit()
+        stmt = select(Transaction).where(Transaction.id == transaction_id)
+        result = await self.db.execute(stmt)
+        transaction = result.scalar_one_or_none()
+        
         if transaction:
-            self.db.delete(transaction)
-            self.db.commit()
+            _ = self.db.delete(transaction)
+            await self.db.commit()
+        
--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
 from typing import Optional
 
 from app.models.user import User
@@ -6,18 +7,26 @@ from app.models.user import User
 
 class UserRepository:
     
-    def __init__(self, db: Session):
+    def __init__(self, db: AsyncSession):
+        
         self.db = db
         
-    def get_by_username(self, username: str) -> Optional[User]:
-        return self.db.query(User).filter(User.username == username).first()
+    async def get_by_username(self, username: str) -> Optional[User]:
+        
+        stmt = select(User).where(User.username == username)
+        result = await self.db.execute(stmt)
+        return result.scalar_one_or_none()
+        
     
-    def get_by_id(self, user_id: int) -> Optional[User]:
-        return self.db.query(User).filter(User.id == user_id).first()
+    async def get_by_id(self, user_id: int) -> Optional[User]:
+        
+        result = await self.db.get(User, user_id)
+        return result
     
-    def create(self, username: str, hashed_password: str) -> User:
+    async def create(self, username: str, hashed_password: str) -> User:
+        
         new_user = User(username=username, hashed_password=hashed_password)
         self.db.add(new_user)
-        self.db.commit()
-        self.db.refresh(new_user)
+        await self.db.commit()
+        await self.db.refresh(new_user)
         return new_user
