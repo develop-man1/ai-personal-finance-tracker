11242973ad6d52f0e857d4d093015d65b4b30acf
develop-man1 <smailhit2003@gmail.com>
2026-02-09 03:45:53 +0500

    add async routes

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.database import get_db
 from app.schemas.token import Token
@@ -16,21 +16,21 @@ router = APIRouter(
 
 
 @router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
-def register(request: UserCreate, db: Session = Depends(get_db)):
+async def register(request: UserCreate, db: AsyncSession = Depends(get_db)):
     
     service = AuthService(db)
     
-    new_user = service.register_user(request.username, request.password)
+    new_user = await service.register_user(request.username, request.password)
     
     return new_user
 
 
 @router.post("/login", response_model=Token, status_code=status.HTTP_200_OK)
-def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
     
     service = AuthService(db)
     
-    user = service.authenticate_user(form_data.username, form_data.password)
+    user = await service.authenticate_user(form_data.username, form_data.password)
     
     access_token = create_access_token(data={"sub": user.username})
     
--- app/routes/users.py
+++ app/routes/users.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, Depends, status
-from sqlalchemy.orm import Session
+from fastapi import APIRouter, Depends
 
 from app.schemas.user import UserResponse
 from app.models.user import User
@@ -13,5 +12,5 @@ router = APIRouter(
 
 
 @router.get("/me", response_model=UserResponse)
-def get_current_user_info(current_user: User = Depends(get_current_user)):
+async def get_current_user_info(current_user: User = Depends(get_current_user)):
     return current_user
