4e4063d5ff5c386c775632a9444fb7404cadcce9
develop-man1 <smailhit2003@gmail.com>
2026-02-11 01:41:34 +0500

    add docker + postgresql

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import declarative_base
+from app.config import settings
 
-engine = create_async_engine("sqlite+aiosqlite:///./finance_tracker.db")
+engine = create_async_engine(settings.database_url, echo=True)
 
 AsyncSessionLocal = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
