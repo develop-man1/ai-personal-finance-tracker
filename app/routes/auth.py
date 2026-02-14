f88164ce193b0b4d1caf65f2f1bc47550acf78bc
develop-man1 <smailhit2003@gmail.com>
2026-02-14 17:39:41 +0500

    add limiter for auth

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, Request, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -7,6 +7,7 @@ from app.schemas.token import Token
 from app.schemas.user import UserCreate, UserResponse
 from app.services.auth_service import AuthService
 from app.utils.security import create_access_token
+from app.middleware.rate_limit import limiter
 
 
 router = APIRouter(
@@ -16,17 +17,19 @@ router = APIRouter(
 
 
 @router.post("/register", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
-async def register(request: UserCreate, db: AsyncSession = Depends(get_db)):
+@limiter.limit("3/minute")
+async def register(request: Request, user_data: UserCreate, db: AsyncSession = Depends(get_db)):
     
     service = AuthService(db)
     
-    new_user = await service.register_user(request.username, request.password)
+    new_user = await service.register_user(user_data.username, user_data.password)
     
     return new_user
 
 
 @router.post("/login", response_model=Token, status_code=status.HTTP_200_OK)
-async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
+@limiter.limit("5/minute")
+async def login(request: Request, form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)):
     
     service = AuthService(db)
     
